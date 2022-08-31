a0c80350b37d36a28db85fe3372d4a3047fc4c95
Anubhav Jaiswal <36488000+anubhavjaiswal03@users.noreply.github.com>
2022-08-31 15:27:57 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Verify Account UI
 
 <p align="center">
-  <img src="http://www.fillmurray.com/460/300">
+  <img src="https://media2.giphy.com/media/pM3f8Gk4F7k4khLiAh/giphy.gif?cid=790b7611b811b7c244c0e5dee25bb137a1518cde44197c92&rid=giphy.gif&ct=g">
 </p>
+
+- Hacky Implementation
+  - needs better ui implementation
+  - needs better flow-through on type
+  - not compatible with mobile devices
--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const codes = document.querySelectorAll('.code');
 
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+	code.addEventListener('keydown', (e) => {
+		if (e.key >= 0 && e.key <= 9) {
+			codes[idx].value = '';
+			if (idx < codes.length - 1) {
+				setTimeout(() => codes[idx + 1].focus(), 1);
+			}
+		} else if (e.key === 'Backspace') {
+			if (idx > 0) {
+				setTimeout(() => codes[idx - 1].focus(), 10);
+				console.log(idx, codes.length);
+			}
+		}
+	});
+});
